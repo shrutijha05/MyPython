15550708d3a3bc30405ee4e3584b72df47d4ba5d
shrutijha05 <64417229+shrutijha05@users.noreply.github.com>
2020-04-29 01:13:35 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-def Write_File(list_passed):
-        list_files=list_passed
-        with open('E:\Output\File_list.xls', "w") as myfile:
-                for l in list_files:
-                        myfile.write("%s\t" % l)
-                        if ((list_files.index(l)%2)!=0):
-                                myfile.write("\n
