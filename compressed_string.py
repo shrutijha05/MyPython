149fb4b80d1ce40d22d093a20073788fcbe73b36
shrutijha05 <64417229+shrutijha05@users.noreply.github.com>
2021-12-21 00:16:20 -0500

    Add files via upload

---
+++ compressed_string.py
@@ -0,0 +1,44 @@
+def decomress(str):
+    nstr=''
+    i=0
+    while(str.count('[')>0):
+        if(str[i])==']':
+            n=str[0:i+1]
+            str=str[i+1:len(str)]
+            if (n.count('[')>1):
+                i=0
+                for x in str:
+                    i=i+1
+                    if x==']':
+                        n=n+str[0:i]
+                        str=str[i+1:len(str)]
+                        m=1
+                        while(m<len(n)):
+                            if not (n[0:m].isnumeric()):
+                                break
+                            m=m+1
+                        c=n[0:m-1]
+                        n=decomress(n[m:len(n)-1])
+                        n=c+'['+n+']'
+            x=1
+            while(x<len(n)):
+                if not (n[0:x].isnumeric()):
+                    break
+                x=x+1
+            
+            i=-1
+            m=n[x:-1]
+            c=int(n[0:x-1])
+            for i in range(0,c):
+                nstr=nstr+m
+
+        i=i+1
+    return(nstr+str)          
+
+str='2[3[a]b]'
+res=decomress(str)
+tres='aaabaaab'
+if (res==tres):
+    print(True)
+else:
+    print(False)
